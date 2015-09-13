fd7085da06383fdf6fdbf191da7d8b4236143c27
Stephen England <thealah@gmail.com>
2015-09-13 13:17:39 -0500

    Added ability to show multiple collections at once

--- www/app/controllers.js
+++ www/app/controllers.js
@@ -11,31 +11,60 @@ bggCollectionsControllers.controller('HomeCtrl', ['$scope', 'Collection',
     $scope.showInGrid = true;
     $scope.showInTable = false;
     $scope.collection = [];
+    $scope.users = {};
+    $scope.addUsername = '';
 
-    var requestCollection = function () {
+    $scope.refreshCollection = function () {
+      var col = {};
+      for (var userKey in $scope.users) {
+        var user = $scope.users[userKey];
+        for (var gameKey in user.collection) {
+          var game = user.collection[gameKey];
+          if (!col[game.objectid]) {
+            col[game.objectid] = game;
+            game.owners = [user.username];
+          }
+          else {
+            col[game.objectid].owners.push(user.username)
+          }
+        }
+      }
+
+      $scope.collection = col;
+    };
+
+    $scope.requestCollection = function (username) {
+      $scope.users[username].loadingCollection = true;
       Collection.get({
-        "username": "nitroscen"
+        "username": username
       }, function (data) {
         if (data.collection && data.collection.length > 0) {
-          $scope.collection = data.collection;
+          $scope.users[username].collection = data.collection;
+          $scope.refreshCollection();
         }
 
         if (data.processing) {
-          setTimeout(requestCollection, 5000);
+          setTimeout(function () {
+            $scope.requestCollection(username);
+          }, 5000);
         }
         else {
-          $scope.loadingCollection = false;
+          $scope.users[username].loadingCollection = false;
         }
       });
     };
-    requestCollection();
-
     $scope.showGrid = function () {
       $scope.showInTable = false;
       $scope.showInGrid = true;
     };
+
     $scope.showTable = function () {
       $scope.showInTable = true;
       $scope.showInGrid = false;
+    };
+
+    $scope.addUser = function () {
+      $scope.users[$scope.addUsername] = { "username": $scope.addUsername };
+      $scope.requestCollection($scope.addUsername); //TODO: Add error handling logic
     };
   }]);
